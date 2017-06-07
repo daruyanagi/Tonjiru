c10ec83aacfe86bf0fe76db57e3c9e222747be20
Hidetoshi Yanagi <daruyanagi@gmail.com>
2017-06-07 23:09:54 +0900

    resolve #16
    通知機能を付けた

---
+++ Tonjiru/Tonjiru/NotificationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonjiru
+{
+    public static class NotificationHelper
+    {
+        public static void ShowBalloonTip()
+        {
+            var notify_icon = new System.Windows.Forms.NotifyIcon();
+            notify_icon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
+            notify_icon.BalloonTipTitle = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            notify_icon.BalloonTipText = "Shutdown signals are sent.";
+            notify_icon.Visible = true;
+            notify_icon.ShowBalloonTip(3000);
+        }
+    }
+}
